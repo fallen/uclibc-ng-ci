7cd93d99c70962438db592184a7d43436c25b001
Yann Sionneau <yann@sionneau.net>
2019-05-28 18:21:18 +0200

    parse testsuite output from qemu and generate junit xml report

--- bin/qemucomm.py
+++ bin/qemucomm.py
@@ -2,8 +2,12 @@
 
 import sys
 import time
+import re
+
+from junit_xml import TestSuite, TestCase
 
 logged_in = False
+test_cases = []
 
 def send(cmd, inpipe):
     print("< {}".format(cmd), end='')
@@ -12,23 +16,52 @@ def send(cmd, inpipe):
 
 def handle(line, inpipe):
     global logged_in
+    global test_cases
     print("> {}".format(line), end='', flush=True)
-    if not logged_in and line.startswith('Welcome to Buildroot'):
-        time.sleep(1)
-        print("we got the prompt!")
-        send("root\n", inpipe)
-        logged_in = True
-        time.sleep(5)
-        send("cd /usr/lib/uclibc-ng-test/test\n", inpipe)
-        send("sh uclibcng-testrunner.sh\n", inpipe)
-    if logged_in and 'Total passed:' in line:
-        print("uClibc-ng testsuite run is over, exiting.")
-        sys.exit(0)
+    if not logged_in:
+        if line.startswith('Welcome to Buildroot'):
+            time.sleep(1)
+            print("we got the prompt!")
+            send("root\n", inpipe)
+            logged_in = True
+            time.sleep(5)
+            send("cd /usr/lib/uclibc-ng-test/test\n", inpipe)
+            send("sh uclibcng-testrunner.sh\n", inpipe)
+    else:
+        if 'PASS ' in line:
+            r = re.match("PASS (.*)", line)
+            if r:
+                test_name = r.group(1)
+                test = TestCase(test_name, '', time.time())
+                test_cases.append(test)
 
+        if 'FAIL ' in line:
+            r = re.match("FAIL (.*)", line)
+            if r:
+                test_name = r.group(1)
+                test = TestCase(test_name, '', time.time())
+                test.add_failure_info(message="FAIL")
+                test_cases.append(test)
+
+        if 'SKIP' in line:
+            r = re.match("SKIP (.*)", line)
+            if r:
+                test_name = r.group(1)
+                test = TestCase(test_name, '', time.time())
+                test.add_skipped_info(message="SKIP")
+                test_cases.append(test)
+
+        if 'Total passed:' in line:
+            print("uClibc-ng testsuite run is over, writing test results and exiting.")
+            return False
+
+    return True
 
 
 def main():
+    global test_cases
     pipename = sys.argv[1]
+    test_output_file = sys.argv[2]
     outpipename = pipename + ".out"
     inpipename = pipename + ".in"
     inpipe = open(inpipename, "w")
@@ -39,7 +72,13 @@ def main():
             if line == '':
                 not_eof = False
                 print("end of file!")
-            handle(line, inpipe)
+            keep_running = handle(line, inpipe)
+            if not keep_running:
+                break
+
+    ts = TestSuite("uClibc-ng testsuite", test_cases)
+    with open(test_output_file, "w+") as to:
+        to.write(TestSuite.to_xml_string([ts]))
 
 if __name__ == "__main__":
     main()
